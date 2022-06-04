dbaa22ebe1d03e998c58bb6b5b2d1d2514503a2d
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-04 01:05:47 +0000

    add apollo loader to base

--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -1,9 +1,11 @@
 import expressLoader from './express';
+import apolloLoader from './apollo';
 
 export default {
   init: async () => {
     const app = expressLoader();
+    const server = await apolloLoader(app);
 
-    return {app};
+    return {app, server};
   },
 };
