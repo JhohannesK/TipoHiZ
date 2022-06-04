77bef3e7e28cb9ae4b58dcbac93d3dfe26843683
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-04 21:41:24 +0000

    add postgresloader to loaders

--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -1,8 +1,10 @@
 import expressLoader from './express';
 import apolloLoader from './apollo';
+import postgresLoader from './postgres';
 
 export default {
   init: async () => {
+    postgresLoader();
     const app = expressLoader();
     const server = await apolloLoader(app);
 
