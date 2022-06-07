0eadc48148e3bb9ad14352543c9cd7d9a6977e8a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-07 23:21:10 +0000

    add jwt secret to config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -6,4 +6,5 @@ export default {
   databaseUser: process.env.DATABASE_USER as string,
   databasePassword: process.env.DATABASE_PASSWORD as string,
   accessOrigin: process.env.ACCESS_ORIGIN as string,
+  jwtSecret: process.env.JWT_SECRET as string,
 };
