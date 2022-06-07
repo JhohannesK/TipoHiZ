a8f8ed1160fa80866fb300aec7d3fe7ad7140491
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-07 23:56:17 +0000

    add salt rounds to conifg

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -7,4 +7,5 @@ export default {
   databasePassword: process.env.DATABASE_PASSWORD as string,
   accessOrigin: process.env.ACCESS_ORIGIN as string,
   jwtSecret: process.env.JWT_SECRET as string,
+  saltRounds: process.env.SALT_ROUNDS as string,
 };
