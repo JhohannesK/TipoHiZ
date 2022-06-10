bbcbcefa60dfcfcad4e26d0ba0d1b12b2835e073
JhohannesK <Jdomeh77@gmail.com>
2022-06-10 04:42:28 +0000

    Deleted and recreated frontend path

---
+++ frontend/tipohiz/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+	return (
+		<div className="bg-slate-800 text-gray-400 font-open-sans h-screen"></div>
+	);
+}
