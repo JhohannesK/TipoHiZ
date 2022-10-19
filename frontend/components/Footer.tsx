c62df36f096b999e7d428c886bb301225eb6e137
JhohannesK <Jdomeh77@gmail.com>
2022-10-19 15:52:38 +0000

    add link to footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
    return (
       <div className="pb-[58px] pt-16 flex items-center justify-center">
-         <div>good</div>
+         <div>
+            <pre className="text-emerald-500 text-sm"> {`\<TipoHiZ/\>`} </pre>
+         </div>
+         <div>
+            <p>
+               Contribute on{' '}
+               <Link href="https://github.com/TipoHiZ">Github</Link>
+            </p>
+         </div>
       </div>
    );
 };
