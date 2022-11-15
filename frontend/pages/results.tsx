8638b029d0c7e072af11f3de366da0bf139ff083
JhohannesK <jdomeh77@gmail.com>
2022-11-15 11:39:37 +0000

    change time constant

--- frontend/pages/results.tsx
+++ frontend/pages/results.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import useStore, { State } from '../store';
+import Constants from '../components/modules/_constants';
+
+const selector = ({ time, setTime }: State) => {
+   return {
+      time: time,
+      setTimer: setTime,
+   };
+};
 
 const results = () => {
    const router = useRouter();
+   const { time, setTimer } = useStore(selector);
    return (
       <div className="">
          This is where the results would be displayed
-         <button onClick={() => router.push('/')}>Go back</button>
+         <button
+            onClick={() => {
+               router.push('/');
+               setTimer(Constants.TimeSec);
+            }}
+         >
+            Go back
+         </button>
       </div>
    );
 };
