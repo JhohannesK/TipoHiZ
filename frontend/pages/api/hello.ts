144da9aaa3c3938de71303dec4ce7cfcb4d33bf5
JhohannesK <jdomeh77@gmail.com>
2022-12-17 19:28:27 +0000

    change hello.ts

--- frontend/pages/api/hello.ts
+++ frontend/pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+   res.status(200).json({ name: 'John Doe' });
 }
