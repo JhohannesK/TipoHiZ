c726a443c386a9d890bb4f96fe48bcb674bf926a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-12-18 20:14:05 -0500

    remove omit util. No longer used

--- server/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const omit = (obj: {[key: string]: unknown}, keyToRemove: string) => {
-  return Object.keys(obj)
-    .filter(key => key !== keyToRemove)
-    .reduce(
-      (res: {[key: string]: unknown}, key) => ((res[key] = obj[key]), res),
-      {}
-    );
-};
