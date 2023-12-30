538447410fd235bf6123e8b1f2894927918771fa
Domeh John Kelvin <75913666+JhohannesK@users.noreply.github.com>
2023-12-30 12:20:01 +0000

    add shake animations (#43)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,9 +15,16 @@ module.exports = {
                '0%, 100%': { opacity: 0 },
                '50%': { opacity: 1 },
             },
+            shake: {
+               '10%, 90%': { transform: ' translate3d(-1px, 0, 0)' },
+               '25%, 75%': { transform: ' translate3d(2px, 0, 0)' },
+               '30%, 50%, 70%': { transform: ' translate3d(-4px, 0, 0)' },
+               '40%, 60%': { transform: 'translate3d(-4px)' },
+            },
          },
          animation: {
             blink: 'blink 1s linear infinite',
+            shake: 'shake 0.8s cubic-bezier(.36, .07, .19, .97) both',
          },
       },
       plugins: [],
