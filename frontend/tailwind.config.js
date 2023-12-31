e77f341bb0be15911eeed0e7d6d21e5ff2b0f245
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-12-31 06:26:39 -0600

    add container tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
    ],
    theme: {
+      container: {
+         center: true,
+         padding: '2rem',
+         screens: {
+            '2xl': '1400px',
+         },
+      },
       extend: {
          fontFamily: {
-            sans: ['var(--font-inconsolata)'],
+            sans: ['var(--font-sans)'],
             mono: ['var(--font-roboto-mono)'],
          },
          keyframes: {
