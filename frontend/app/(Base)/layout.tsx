c6bb9c0ae04f1a0a6acf9146d9822f91f08dd33e
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-12-31 06:28:01 -0600

    base - layout

---
+++ frontend/app/(Base)/layout.tsx
@@ -0,0 +1,12 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+
+export default function BaseLayout({ children }: React.PropsWithChildren) {
+   return (
+      <div className="flex min-h-screen flex-col container">
+         <Header />
+         <main className="flex-1">{children}</main>
+         <Footer />
+      </div>
+   );
+}
