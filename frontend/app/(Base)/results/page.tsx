61012eaf6b858f7fd7d7be16f7a4fd6d722ea1a7
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-12-31 06:28:29 -0600

    add results page boilerplate

---
+++ frontend/app/(Base)/results/page.tsx
@@ -0,0 +1,14 @@
+import Constants from '@/modules/_constants';
+import { setTime } from '@/store/actions/TimeActions';
+import Link from 'next/link';
+
+export default function ResultsPage() {
+   // TODO: Call this function after click -> cannot be done here -> This is a server component
+   // setTime(Constants.TimeSec);
+   return (
+      <div>
+         <h1>This is where the results would be displayed</h1>
+         <Link href={'/'}>Go back</Link>
+      </div>
+   );
+}
