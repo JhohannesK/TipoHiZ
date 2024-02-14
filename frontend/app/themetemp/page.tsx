a2a11880ba805aa8c123ee04d660d0c631ad9b7f
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2024-02-13 20:27:19 -0600

    add theme display test page

---
+++ frontend/app/themetemp/page.tsx
@@ -0,0 +1,32 @@
+// TODO: Delete after theme integration
+// ** This is just to show how it works
+
+import ThemeChoose from '@/components/expo/theme-choose';
+
+export default function Page() {
+   return (
+      <>
+         <div className="flex items-center justify-center">
+            <h1 className="font-bold text-4xl text-foreground">Theme Expo</h1>
+         </div>
+         <div className="flex items-center justify-center text-foreground">
+            <div className="w-80 rounded m-11 p-4 border-2 border-border bg-card">
+               <h2 className="font-bold text-xl">Payment Method</h2>
+               <p className="text-sm">
+                  Add a new payment method to your account
+               </p>
+               <button
+                  type="submit"
+                  className="flex font-bold items-center justify-center bg-primary w-full rounded py-2 mt-5 text-background"
+               >
+                  <span>submit</span>
+               </button>
+            </div>
+         </div>
+
+         <div>
+            <ThemeChoose />
+         </div>
+      </>
+   );
+}
