815316c5df9252a43b70113cec49f4df0726fe60
jhohannesK <jdomeh77@gmail.com>
2024-02-16 15:20:30 +0000

    add the dropdown pallete for mobile

--- components/UI/Chip.tsx
+++ components/UI/Chip.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
-const Chip = ({ name }: { name: string }) => {
+const Chip = ({
+   name,
+   onClick,
+   className,
+   icon,
+}: {
+   name: string;
+   onClick?: () => void;
+   icon?: React.ReactNode;
+   className?: HTMLAttributes<HTMLDivElement>['className'];
+}) => {
    return (
-      <div className="px-[4px] font-medium bg-purple-400 rounded-lg w-fit">
-         {name}
+      <div
+         onClick={onClick}
+         className={
+            className +
+            ' ' +
+            'px-[4px] font-medium bg-purple-400 rounded-lg w-fit'
+         }
+      >
+         <div className="flex items-center justify-center gap-3">
+            {icon}
+            {name}
+         </div>
       </div>
    );
 };
