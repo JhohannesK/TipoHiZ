39d8b8a40731c45f92e33acd8a95d9ed17c4e8c0
prathap v <vprathap585@gmail.com>
2024-10-05 18:17:07 +0530

    Enhance login signup buttons (#134)
    * Enhance login/signup buttons: Improved alignment
    
    * Enhance login/signup buttons: Improved alignment
    
    * fix: use theme color for text contrast

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaUserAlt } from 'react-icons/fa';
+import { LuUser } from "react-icons/lu";
 import { SvgRender } from './SvgRender';
 
 function Header() {
@@ -11,9 +11,15 @@ function Header() {
                TipoHiz.__
             </p>
          </div>
-         <div className="flex items-center justify-center p-2 px-2 rounded-full gap-x-5 bg-foreground">
-            <FaUserAlt className="text-input" />
-            <p className="text-sm tracking-wide text-input">Login</p>
+         <div className='flex justify-between items-center gap-3 font-semibold'>
+            <div className="flex items-center justify-center p-2 rounded-xl gap-3 cursor-pointer text-foreground bg-input">
+               <p className="text-sm tracking-wide text-background">Login</p>
+               <LuUser className='text-background' />
+            </div>
+            <div className="flex items-center justify-center p-2 rounded-xl bg-foreground gap-3 cursor-pointer">
+               <p className="text-sm tracking-wide text-input">Sign up</p>
+               <LuUser className='text-input'/>
+            </div>
          </div>
       </div>
    );
