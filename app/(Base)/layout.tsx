b29d7b106727061c71bcbaf0ea315f8a59f6d3c1
John Kelvin <jdomeh77@gmail.com>
2025-02-24 23:18:30 +0000

    Rename components/UI to components/ui

--- app/(Base)/layout.tsx
+++ app/(Base)/layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Footer from '@/components/footer';
-import Header from '@/components/header';
+import Footer from '@/components/Footer';
 import AuthSessionProvider from '@/lib/providers/session-provider';
+import Header from '@/components/Header';
 
 export default async function BaseLayout({
    children,
 }: React.PropsWithChildren) {
    return (
       <AuthSessionProvider>
-         <div className="flex min-h-screen flex-col container">
+         <div className="container flex flex-col min-h-screen">
             <Header />
             <main className="flex-1">{children}</main>
             <Footer />
